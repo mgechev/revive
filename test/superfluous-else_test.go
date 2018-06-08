cbe45ffc797a63287b888eeb88b33abbdb84888a
chavacava <salvadorcavadini+github@gmail.com>
2018-06-08 16:06:29 +0200

    Adds rule superfluous-else (an extension of indent-error-flow) (#13)
    * Adds rule superfluous-else (an extension of indent-error-flow)
    
    * Fix superfluous-else rule struct namming.
    
    * Adds superfuous-else rule to the rules table

---
+++ test/superfluous-else_test.go
@@ -0,0 +1,12 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/mgechev/revive/rule"
+)
+
+// TestSuperfluousElse rule.
+func TestSuperfluousElse(t *testing.T) {
+	testRule(t, "superfluous-else", &rule.SuperfluousElseRule{})
+}
