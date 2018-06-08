affffd0b5e0aa08dd1e97d298eaff52d14dde03b
Salvador Cavadini <salvador.cavadini.ext@astrium.eads.net>
2018-06-08 12:57:42 +0200

    empty-block working version

---
+++ fixtures/empty-block.go
@@ -0,0 +1,33 @@
+// Test of empty-blocks.
+
+package fixtures
+
+func f(x int) bool { // MATCH /this block is empty, you can remove it/
+
+}
+
+func g(f func() bool) string {
+	{ // MATCH /this block is empty, you can remove it/
+	}
+
+	if ok := f(); ok { // MATCH /this block is empty, you can remove it/
+		// only a comment
+	} else {
+		println("it's NOT empty!")
+	}
+
+	if ok := f(); ok { // MATCH /this block is empty, you can remove it/
+		println("it's NOT empty!")
+	} else { // MATCH /this block is empty, you can remove it/
+
+	}
+
+	for i := 0; i < 10; i++ { // MATCH /this block is empty, you can remove it/
+
+	}
+
+	for { // MATCH /this block is empty, you can remove it/
+
+	}
+
+}
--- rule/empty-block.go
+++ rule/empty-block.go
@@ -2,79 +2,50 @@ package rule
 
 import (
 	"go/ast"
-	"go/token"
 
 	"github.com/mgechev/revive/lint"
 )
 
-// SuperfluousElse lints given else constructs.
-type SuperfluousElse struct{}
+// EmptyBlockRule lints given else constructs.
+type EmptyBlockRule struct{}
 
 // Apply applies the rule to given file.
-func (r *SuperfluousElse) Apply(file *lint.File, arguments lint.Arguments) []lint.Failure {
+func (r *EmptyBlockRule) Apply(file *lint.File, arguments lint.Arguments) []lint.Failure {
 	var failures []lint.Failure
 
 	onFailure := func(failure lint.Failure) {
 		failures = append(failures, failure)
 	}
 
-	w := lintSuperfluousElse{make(map[*ast.IfStmt]bool), onFailure}
+	w := lintEmptyBlock{make(map[*ast.IfStmt]bool), onFailure}
 	ast.Walk(w, file.AST)
 	return failures
 }
 
 // Name returns the rule name.
-func (r *SuperfluousElse) Name() string {
-	return "superfluous-else"
+func (r *EmptyBlockRule) Name() string {
+	return "empty-block"
 }
 
-type lintSuperfluousElse struct {
+type lintEmptyBlock struct {
 	ignore    map[*ast.IfStmt]bool
 	onFailure func(lint.Failure)
 }
 
-func (w lintSuperfluousElse) Visit(node ast.Node) ast.Visitor {
-	ifStmt, ok := node.(*ast.IfStmt)
-	if !ok || ifStmt.Else == nil {
+func (w lintEmptyBlock) Visit(node ast.Node) ast.Visitor {
+	block, ok := node.(*ast.BlockStmt)
+	if !ok {
 		return w
-	}
-	if w.ignore[ifStmt] {
-		return w
-	}
-	if elseif, ok := ifStmt.Else.(*ast.IfStmt); ok {
-		w.ignore[elseif] = true
-		return w
-	}
-	if _, ok := ifStmt.Else.(*ast.BlockStmt); !ok {
-		// only care about elses without conditions
-		return w
-	}
-	if len(ifStmt.Body.List) == 0 {
-		return w
-	}
-	shortDecl := false // does the if statement have a ":=" initialization statement?
-	if ifStmt.Init != nil {
-		if as, ok := ifStmt.Init.(*ast.AssignStmt); ok && as.Tok == token.DEFINE {
-			shortDecl = true
-		}
-	}
-	extra := ""
-	if shortDecl {
-		extra = " (move short variable declaration to its own line if necessary)"
 	}
 
-	lastStmt := ifStmt.Body.List[len(ifStmt.Body.List)-1]
-	if stmt, ok := lastStmt.(*ast.BranchStmt); ok {
-		token := stmt.Tok.String()
-		if token != "fallthrough" {
-			w.onFailure(lint.Failure{
-				Confidence: 1,
-				Node:       ifStmt.Else,
-				Category:   "indent",
-				URL:        "#indent-error-flow",
-				Failure:    "if block ends with a " + token + " statement, so drop this else and outdent its block" + extra,
-			})
-		}
+	if len(block.List) == 0 {
+		w.onFailure(lint.Failure{
+			Confidence: 1,
+			Node:       block,
+			Category:   "logic",
+			URL:        "#empty-block",
+			Failure:    "this block is empty, you can remove it",
+		})
 	}
 
 	return w
