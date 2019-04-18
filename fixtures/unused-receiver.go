f939d9f696858d6a69354e054756476ac48fefb8
chavacava <salvadorcavadini+github@gmail.com>
2019-04-18 19:14:11 +0200

    unused-receiver: adds more test cases

--- fixtures/unused-receiver.go
+++ fixtures/unused-receiver.go
@@ -16,3 +16,28 @@ func (f *Unix) Format(failures <-chan lint.Failure, _ lint.RulesConfig) (string,
 	}
 	return "", nil
 }
+
+func (u *Unix) Foo() (string, error) { // MATCH /method receiver 'u' is not referenced in method's body, consider removing or renaming it as _/
+	for failure := range failures {
+		u := 1 // shadowing the receiver
+		fmt.Printf("%v\n", u)
+	}
+	return "", nil
+}
+
+func (u *Unix) Foos() (string, error) {
+	for failure := range failures {
+		u := 1 // shadowing the receiver
+		fmt.Printf("%v\n", u)
+	}
+
+	return u, nil // use of the receiver
+}
+
+func (u *Unix) Bar() (string, error) {
+	for failure := range failures {
+		u.path = nil // modifies the receiver
+		fmt.Printf("%v\n", u)
+	}
+	return "", nil
+}
