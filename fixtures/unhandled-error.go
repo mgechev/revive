94b339284e332742c87580c00286ac00f5770104
chavacava <salvadorcavadini+github@gmail.com>
2019-04-21 21:54:59 +0200

    adds test

--- fixtures/unhandled-error.go
+++ fixtures/unhandled-error.go
@@ -11,8 +11,9 @@ func unhandledError1(a int) (int, error) {
 
 func unhandledError2() error {
 	_, err := unhandledError1(1)
-	unhandledError1(1)
-	fmt.Fprintf(nil, "")
-	os.Chdir("..")
+	unhandledError1(1)   // MATCH /Unhandled error in call to function unhandledError1/
+	fmt.Fprintf(nil, "") // MATCH /Unhandled error in call to function &{fmt Fprintf}/
+	os.Chdir("..")       // MATCH /Unhandled error in call to function &{os Chdir}/
+	_ = os.Chdir("..")
 	return err
 }
