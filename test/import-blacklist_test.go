e8c1baf8ac0e6b6cfbd041b0d10ea6aa93f654e6
SalvadorC <salvadorcavadini+github@gmail.com>
2019-04-22 19:58:02 +0200

    optimizes import-blacklist (#123)

--- test/import-blacklist_test.go
+++ test/import-blacklist_test.go
@@ -14,3 +14,13 @@ func TestImportsBlacklist(t *testing.T) {
 		Arguments: args,
 	})
 }
+
+func BenchmarkImportsBlacklist(b *testing.B) {
+	args := []interface{}{"crypto/md5", "crypto/sha1"}
+	var t *testing.T
+	for i := 0; i <= b.N; i++ {
+		testRule(t, "imports-blacklist", &rule.ImportsBlacklistRule{}, &lint.RuleConfig{
+			Arguments: args,
+		})
+	}
+}
