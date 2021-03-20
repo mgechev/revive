46f65914e3b26f7044ffe6842ecce10b58e2c2aa
SalvadorC <salvadorcavadini+github@gmail.com>
2021-03-20 23:45:30 +0100

    new rule function-length (#504)

---
+++ test/function-length_test.go
@@ -0,0 +1,26 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/mgechev/revive/lint"
+	"github.com/mgechev/revive/rule"
+)
+
+func TestFuncLengthLimitsStatements(t *testing.T) {
+	testRule(t, "function-length1", &rule.FunctionLength{}, &lint.RuleConfig{
+		Arguments: []interface{}{int64(2), int64(100)},
+	})
+}
+
+func TestFuncLengthLimitsLines(t *testing.T) {
+	testRule(t, "function-length2", &rule.FunctionLength{}, &lint.RuleConfig{
+		Arguments: []interface{}{int64(100), int64(5)},
+	})
+}
+
+func TestFuncLengthLimitsDeactivated(t *testing.T) {
+	testRule(t, "function-length3", &rule.FunctionLength{}, &lint.RuleConfig{
+		Arguments: []interface{}{int64(0), int64(0)},
+	})
+}
