2e969e47077d69284d13c5e4a14343c8d477c8d6
chavacava <salvadorcavadini+github@gmail.com>
2022-10-07 18:29:58 +0200

    Update unhandled-error-w-ignorelist.go

--- testdata/unhandled-error-w-ignorelist.go
+++ testdata/unhandled-error-w-ignorelist.go
@@ -13,6 +13,7 @@ func unhandledError1(a int) (int, error) {
 func unhandledError2() error {
 	_, err := unhandledError1(1)
 	unhandledError1(1)
+	prefixunhandledError1suffix(1)
 	fmt.Fprintf(nil, "") // MATCH /Unhandled error in call to function fmt.Fprintf/
 	net.Dial("tcp", "127.0.0.1")
 	net.ResolveTCPAddr("tcp4", "localhost:8080")
