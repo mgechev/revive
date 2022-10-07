2e8de0863c6f38a329518546f2aeb98fd411c2de
chavacava <salvadorcavadini+github@gmail.com>
2022-10-07 18:38:28 +0200

    Update unhandled-error_test.go

--- test/unhandled-error_test.go
+++ test/unhandled-error_test.go
@@ -12,7 +12,7 @@ func TestUnhandledError(t *testing.T) {
 }
 
 func TestUnhandledErrorWithBlacklist(t *testing.T) {
-	args := []interface{}{".Chdir", "unhandledError1", "net.*"}
+	args := []interface{}{"os.Chdir", "unhandledError1", "net.*"}
 
 	testRule(t, "unhandled-error-w-ignorelist", &rule.UnhandledErrorRule{}, &lint.RuleConfig{Arguments: args})
 }
