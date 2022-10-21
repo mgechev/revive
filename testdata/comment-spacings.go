56973af4cb201215824523d55cb46ad88e10f7a0
chavacava <salvadorcavadini+github@gmail.com>
2022-10-21 17:37:55 +0000

    adds some tests

--- testdata/comment-spacings.go
+++ testdata/comment-spacings.go
@@ -1,9 +1,22 @@
 package commentspacings
 
-//go:embed
-//go:linkname
+//revive:disable:cyclomatic
+type some struct{}
 
-// This is a comment. 
-type hello struct{
-	random `json:random` //This is invalid  MATCH /no space between comment symbol and comment text/
+//revive:disable:cyclomatic High complexity score but easy to understand
+type some1 struct{}
+
+// Some nice comment
+//
+// With an empty line in the middle will make the rule panic!
+func itsATrap() {}
+
+// This is a well formed comment
+type hello struct {
+	random `json:random`
 }
+
+// MATCH:18 /no space between comment symbol and comment text/
+
+//This comment does not respect the spacing rule!
+var a string
