7b1458a9cb2d0b92aa351aa53e86fa16f4598f37
Buyanov Vladimir <v.buianov@gaijin.team>
2022-11-09 15:13:52 +0300

    fix #754 [rule.unhandled-error] change arguments to regexp (#757)
    * fix #754 [rule.unhandled-error] change arguments to regexp
    
    * Update unhandled-error-w-ignorelist.go
    
    * Update unhandled-error_test.go
    
    * Update unhandled-error-w-ignorelist.go
    
    * adds config backward compatibility test
    
    * fix #754 [rule.unhandled-error] change arguments to regexp
    
    * fix #754 [rule.unhandled-error] change arguments to regexp
    
    * fix #754 [rule.unhandled-error] change arguments to regexp
    
    Co-authored-by: chavacava <salvadorcavadini+github@gmail.com>

--- RULES_DESCRIPTIONS.md
+++ RULES_DESCRIPTIONS.md
@@ -670,13 +670,13 @@ _Configuration_: N/A
 
 _Description_: This rule warns when errors returned by a function are not explicitly handled on the caller side.
 
-_Configuration_: function names to ignore
+_Configuration_: function names regexp patterns to ignore
 
 Example:
 
 ```toml
 [unhandled-error]
-  arguments =["fmt.Printf", "myFunction"]
+  arguments =["os\.(Create|WriteFile|Chmod)", "fmt\.Print", "myFunction", "net\..*", "bytes\.Buffer\.Write"]
 ```
 ## unnecessary-stmt
 
--- test/unhandled-error_test.go
+++ test/unhandled-error_test.go
@@ -11,8 +11,15 @@ func TestUnhandledError(t *testing.T) {
 	testRule(t, "unhandled-error", &rule.UnhandledErrorRule{})
 }
 
-func TestUnhandledErrorWithBlacklist(t *testing.T) {
-	args := []interface{}{"os.Chdir", "unhandledError1"}
+func TestUnhandledErrorWithIgnoreList(t *testing.T) {
+	args := []interface{}{
+		`unhandledError1`,
+		`fmt\.Print`,
+		`os\.(Create|WriteFile|Chmod)`,
+		`net\..*`,
+		`bytes\.Buffer\.Write`,
+		`fixtures\.unhandledErrorStruct2\.reterr`,
+	}
 
 	testRule(t, "unhandled-error-w-ignorelist", &rule.UnhandledErrorRule{}, &lint.RuleConfig{Arguments: args})
 }
