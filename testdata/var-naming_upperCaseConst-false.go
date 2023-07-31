fb0b16024a274fe3c145da8e0e4e60a1f0d966c5
comdiv <fagim.sadykov@gmail.com>
2023-07-31 11:53:16 +0500

    fix test

--- testdata/var-naming_upperCaseConst-false.go
+++ testdata/var-naming_upperCaseConst-false.go
@@ -6,7 +6,4 @@ const SOME_CONST_2 = 1 // MATCH /don't use ALL_CAPS in Go names; use CamelCase/
 
 const (
 	SOME_CONST_3 = 3 // MATCH /don't use ALL_CAPS in Go names; use CamelCase/
-	// DUE TO LEGACY - names less 5 symbols without undescores are not treated as UPPER_CASE
-	// It's strange, but VERYLARGENAME is not linted too
-	VER = 0
 )
