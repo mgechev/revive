7279db757308b14fc9b5f97f55d2cbbca9e0eebb
chavacava <salvadorcavadini+github@gmail.com>
2023-08-21 16:24:54 +0200

    adds test case with local var named as a function

--- testdata/import-shadowing.go
+++ testdata/import-shadowing.go
@@ -23,7 +23,9 @@ type fmt interface {} // MATCH /The name 'fmt' shadows an import name/
 
 func (ast myAst) foo() {} // MATCH /The name 'ast' shadows an import name/
 
-func (a myAst) fmt() {} // this should be skipped (method, not a pkg func)
+func (a myAst) fmt() { // this should be skipped (method, not a pkg func)
+	var fmt string // MATCH /The name 'fmt' shadows an import name/
+}
 
 func (a myAst) md5() { // this should be skipped (method, not a pkg func)
 	strings := map[string]string{} // MATCH /The name 'strings' shadows an import name/
