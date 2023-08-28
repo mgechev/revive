3a11e01c9eaf96b0ee79168684570353a4ff27a2
Denis Voytyuk <5462781+denisvmedia@users.noreply.github.com>
2023-08-28 14:52:58 +0200

    Add positive test expression

--- testdata/import-alias-naming-custom-config.go
+++ testdata/import-alias-naming-custom-config.go
@@ -5,10 +5,12 @@ import (
 	bar_foo "strings" // MATCH /import name (bar_foo) must match the regular expression: ^[a-z]+$/
 	fooBAR "strings"  // MATCH /import name (fooBAR) must match the regular expression: ^[a-z]+$/
 	v1 "strings"      // MATCH /import name (v1) must match the regular expression: ^[a-z]+$/
+	magical "magic/hat"
 )
 
 func somefunc() {
 	fooBAR.Clone("")
 	bar_foo.Clone("")
 	v1.Clone("")
+	magical.Clone("")
 }
