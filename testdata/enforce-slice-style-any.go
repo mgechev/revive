0f7854393fe81c38683159d8dbf8d24e6da0db93
Denis Vojtuk <5462781+denisvmedia@users.noreply.github.com>
2023-09-23 07:33:27 +0200

    Add more tests in any

--- testdata/enforce-slice-style-any.go
+++ testdata/enforce-slice-style-any.go
@@ -4,10 +4,11 @@ func somefn() {
 	m0 := make([]string, 10)
 	m1 := make([]string, 0, 10)
 	m2 := make([]string, 0)
-	m3 := []string{}
-	m4 := []string{"v1", "v2"}
-	m5 := [8]string{}
-	m6 := [...]string{}
+	m3 := make([]string, 0, 0)
+	m4 := []string{}
+	m5 := []string{"v1", "v2"}
+	m6 := [8]string{}
+	m7 := [...]string{}
 
 	_ = m0
 	_ = m1
@@ -16,6 +17,7 @@ func somefn() {
 	_ = m4
 	_ = m5
 	_ = m6
+	_ = m7
 }
 
 type Slice []string
@@ -24,30 +26,34 @@ func somefn2() {
 	m0 := make(Slice, 10)
 	m1 := make(Slice, 0, 10)
 	m2 := make(Slice, 0)
-	m3 := Slice{}
-	m4 := Slice{"v1", "v2"}
+	m3 := make(Slice, 0, 0)
+	m4 := Slice{}
+	m5 := Slice{"v1", "v2"}
 
 	_ = m0
 	_ = m1
 	_ = m2
 	_ = m3
 	_ = m4
+	_ = m5
 }
 
 type SliceSlice Slice
 
 func somefn3() {
+	m2 := make(SliceSlice, 0)
+	m3 := make(SliceSlice, 0, 0)
 	m0 := make(SliceSlice, 10)
 	m1 := make(SliceSlice, 0, 10)
-	m2 := make(SliceSlice, 0)
-	m3 := SliceSlice{}
-	m4 := SliceSlice{"v1", "v2"}
+	m4 := SliceSlice{}
+	m5 := SliceSlice{"v1", "v2"}
 
 	_ = m0
 	_ = m1
 	_ = m2
 	_ = m3
 	_ = m4
+	_ = m5
 }
 
 func somefn4() {
@@ -55,8 +61,9 @@ func somefn4() {
 	m1["el0"] = make([]string, 10)
 	m1["el1"] = make([]string, 0, 10)
 	m1["el2"] = make([]string, 0)
-	m1["el3"] = []string{}
-	m1["el4"] = []string{"v1", "v2"}
+	m1["el3"] = make([]string, 0, 0)
+	m1["el4"] = []string{}
+	m1["el5"] = []string{"v1", "v2"}
 
 	_ = m1
 }
