a6df12abb18c484c6110fcb8fb2d726c03fb6d14
chavacava <salvadorcavadini+github@gmail.com>
2023-09-24 08:50:28 +0200

    removes unused receiver

--- rule/enforce-map-style.go
+++ rule/enforce-map-style.go
@@ -141,7 +141,7 @@ func (r *EnforceMapStyleRule) Apply(file *lint.File, arguments lint.Arguments) [
 }
 
 // Name returns the rule name.
-func (_ *EnforceMapStyleRule) Name() string {
+func (*EnforceMapStyleRule) Name() string {
 	return "enforce-map-style"
 }
 
