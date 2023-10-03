70273b50ab59777940c9aa07bed85004746d143f
Marcin Federowicz <marcin.federowicz@gmail.com>
2023-10-03 12:21:35 +0200

    test if xdg config dir is preffered first

--- cli/main_test.go
+++ cli/main_test.go
@@ -8,19 +8,45 @@ import (
 )
 
 func init() {
+	os.Unsetenv("HOME")
+	os.Unsetenv("XDG_CONFIG_HOME")
+	AppFs = afero.NewMemMapFs()
+}
 
-    AppFs = afero.NewMemMapFs()
+func TestXDGConfigDirIsPrefferedFirst(t *testing.T) {
+
+	xdgDirPath := "/tmp-iofs/xdg/config"
+	homeDirPath := "/tmp-iofs/home/tester"
+	AppFs.MkdirAll(xdgDirPath, 0755)
+	AppFs.MkdirAll(homeDirPath, 0755)
+
+	afero.WriteFile(AppFs, xdgDirPath+"/revive.toml", []byte("\n"), 0644)
+	os.Setenv("XDG_CONFIG_HOME", xdgDirPath)
+
+	afero.WriteFile(AppFs, homeDirPath+"/revive.toml", []byte("\n"), 0644)
+	os.Setenv("HOME", homeDirPath)
+
+	got := buildDefaultConfigPath()
+	want := xdgDirPath + "/revive.toml"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+
+	//reset fs after test
+	AppFs = afero.NewMemMapFs()
+
 }
 
 func TestHomeConfigDir(t *testing.T) {
-	
-    homeDirPath := "/tmp-iofs/home/tester"
+
+	homeDirPath := "/tmp-iofs/home/tester"
 	AppFs.MkdirAll(homeDirPath, 0755)
 
 	afero.WriteFile(AppFs, homeDirPath+"/revive.toml", []byte("\n"), 0644)
 	os.Setenv("HOME", homeDirPath)
-	
-    got := buildDefaultConfigPath()
+
+	got := buildDefaultConfigPath()
 	want := homeDirPath + "/revive.toml"
 
 	if got != want {
@@ -49,9 +75,9 @@ func TestXDGConfigDir(t *testing.T) {
 func TestXDGConfigDirNoFile(t *testing.T) {
 
 	xdgDirPath := "/tmp-iofs/xdg/config"
-    os.Setenv("XDG_CONFIG_HOME", xdgDirPath)
-    
-    got := buildDefaultConfigPath()
+	os.Setenv("XDG_CONFIG_HOME", xdgDirPath)
+
+	got := buildDefaultConfigPath()
 	want := xdgDirPath + "/revive.toml"
 
 	if got != want {
