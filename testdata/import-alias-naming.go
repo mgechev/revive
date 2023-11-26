07eb59669a71ae6cffe78f4de65f0d696eaada62
Denis Voytyuk <5462781+denisvmedia@users.noreply.github.com>
2023-11-26 08:57:34 +0100

    fix: ignore dot import aliases in import-alias-naming rule (#938)

--- testdata/import-alias-naming.go
+++ testdata/import-alias-naming.go
@@ -1,7 +1,8 @@
 package fixtures
 
 import (
-	_ "strings"
+	_ "strings" // _ aliases should be ignored
+	. "dotimport" // . aliases should be ignored
 	bar_foo "strings" // MATCH /import name (bar_foo) must match the regular expression: ^[a-z][a-z0-9]{0,}$/
 	fooBAR "strings"  // MATCH /import name (fooBAR) must match the regular expression: ^[a-z][a-z0-9]{0,}$/
 	v1 "strings"
