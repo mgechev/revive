9d73a9fa9483d150f2ac480dcd7a5125d041a3c6
Marcin Federowicz <marcin.federowicz@gmail.com>
2023-12-14 13:45:57 +0100

    revert blacklist test and testdata

---
+++ test/import-blacklist_test.go
@@ -0,0 +1,34 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/mgechev/revive/lint"
+	"github.com/mgechev/revive/rule"
+)
+
+func TestImportsBlacklistOriginal(t *testing.T) {
+	args := []any{"crypto/md5", "crypto/sha1"}
+
+	testRule(t, "imports-blacklist-original", &rule.ImportsBlacklistRule{}, &lint.RuleConfig{
+		Arguments: args,
+	})
+}
+
+func TestImportsBlacklist(t *testing.T) {
+	args := []any{"github.com/full/match", "wildcard/**/between", "wildcard/backward/**", "**/wildcard/forward", "full"}
+
+	testRule(t, "imports-blacklist", &rule.ImportsBlacklistRule{}, &lint.RuleConfig{
+		Arguments: args,
+	})
+}
+
+func BenchmarkImportsBlacklist(b *testing.B) {
+	args := []any{"github.com/full/match", "wildcard/**/between", "wildcard/backward/**", "**/wildcard/forward", "full"}
+	var t *testing.T
+	for i := 0; i <= b.N; i++ {
+		testRule(t, "imports-blacklist", &rule.ImportsBlacklistRule{}, &lint.RuleConfig{
+			Arguments: args,
+		})
+	}
+}
---
+++ testdata/imports-blacklist-original.go
@@ -0,0 +1,8 @@
+package fixtures
+
+import (
+	"crypto/md5"  //  MATCH /should not use the following blacklisted import: "crypto/md5"/
+	"crypto/sha1" // MATCH /should not use the following blacklisted import: "crypto/sha1"/
+	"strings"
+)
+
