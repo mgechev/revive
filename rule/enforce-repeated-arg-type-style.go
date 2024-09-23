a9ead2364ce70f172764399de11b54171beabc68
chavacava <salvador.cavadini@gmail.com>
2024-09-23 21:40:08 +0200

    fix #1032 by comparing string representations of types

--- rule/enforce-repeated-arg-type-style.go
+++ rule/enforce-repeated-arg-type-style.go
@@ -3,8 +3,6 @@ package rule
 import (
 	"fmt"
 	"go/ast"
-	"go/types"
-	"strings"
 	"sync"
 
 	"github.com/mgechev/revive/lint"
@@ -105,13 +103,6 @@ func (r *EnforceRepeatedArgTypeStyleRule) Apply(file *lint.File, arguments lint.
 
 	var failures []lint.Failure
 
-	err := file.Pkg.TypeCheck()
-	if err != nil {
-		// the file has other issues
-		return nil
-	}
-	typesInfo := file.Pkg.TypesInfo()
-
 	astFile := file.AST
 	ast.Inspect(astFile, func(n ast.Node) bool {
 		switch fn := n.(type) {
@@ -135,13 +126,14 @@ func (r *EnforceRepeatedArgTypeStyleRule) Apply(file *lint.File, arguments lint.
 				var prevType ast.Expr
 				if fn.Type.Params != nil {
 					for _, field := range fn.Type.Params.List {
-						// TODO: For invalid types we could have compared raw import names (import package alias + selector), but will it work properly in all the cases?
-						if !r.isInvalidType(typesInfo.Types[field.Type].Type) && types.Identical(typesInfo.Types[field.Type].Type, typesInfo.Types[prevType].Type) {
+						prevTypeStr := gofmt(prevType)
+						currentTypeStr := gofmt(field.Type)
+						if currentTypeStr == prevTypeStr {
 							failures = append(failures, lint.Failure{
 								Confidence: 1,
-								Node:       field,
+								Node:       prevType,
 								Category:   "style",
-								Failure:    "repeated argument type can be omitted",
+								Failure:    fmt.Sprintf("repeated argument type %q can be omitted", prevTypeStr),
 							})
 						}
 						prevType = field.Type
@@ -168,13 +160,14 @@ func (r *EnforceRepeatedArgTypeStyleRule) Apply(file *lint.File, arguments lint.
 				var prevType ast.Expr
 				if fn.Type.Results != nil {
 					for _, field := range fn.Type.Results.List {
-						// TODO: For invalid types we could have compared raw import names (import package alias + selector), but will it work properly in all the cases?
-						if !r.isInvalidType(typesInfo.Types[field.Type].Type) && field.Names != nil && types.Identical(typesInfo.Types[field.Type].Type, typesInfo.Types[prevType].Type) {
+						prevTypeStr := gofmt(prevType)
+						currentTypeStr := gofmt(field.Type)
+						if field.Names != nil && currentTypeStr == prevTypeStr {
 							failures = append(failures, lint.Failure{
 								Confidence: 1,
-								Node:       field,
+								Node:       prevType,
 								Category:   "style",
-								Failure:    "repeated return type can be omitted",
+								Failure:    fmt.Sprintf("repeated return type %q can be omitted", prevTypeStr),
 							})
 						}
 						prevType = field.Type
@@ -191,11 +184,4 @@ func (r *EnforceRepeatedArgTypeStyleRule) Apply(file *lint.File, arguments lint.
 // Name returns the name of the linter rule.
 func (*EnforceRepeatedArgTypeStyleRule) Name() string {
 	return "enforce-repeated-arg-type-style"
-}
-
-// Invalid types are imported from other packages, and we can't compare them.
-// Note, we check the string suffix to cover all the cases: non-pointer, pointer, double pointer, etc.
-// See: https://github.com/mgechev/revive/issues/1032
-func (*EnforceRepeatedArgTypeStyleRule) isInvalidType(t types.Type) bool {
-	return strings.HasSuffix(t.String(), "invalid type")
 }
