14babf2824a95a27a0221a7a5afd78d37551b584
Oleksandr Redko <oleksandr.red+github@gmail.com>
2024-10-28 14:22:36 +0200

    refactor: move lint.Name to name.go file (#1084)

--- test/utils_test.go
+++ test/utils_test.go
@@ -288,47 +288,6 @@ func TestLine(t *testing.T) { //revive:disable-line:exported
 	}
 }
 
-// TestLintName tests lint.Name function
-func TestLintName(t *testing.T) { //revive:disable-line:exported
-	tests := []struct {
-		name, want string
-	}{
-		{"foo_bar", "fooBar"},
-		{"foo_bar_baz", "fooBarBaz"},
-		{"Foo_bar", "FooBar"},
-		{"foo_WiFi", "fooWiFi"},
-		{"id", "id"},
-		{"Id", "ID"},
-		{"foo_id", "fooID"},
-		{"fooId", "fooID"},
-		{"fooUid", "fooUID"},
-		{"idFoo", "idFoo"},
-		{"uidFoo", "uidFoo"},
-		{"midIdDle", "midIDDle"},
-		{"APIProxy", "APIProxy"},
-		{"ApiProxy", "APIProxy"},
-		{"apiProxy", "apiProxy"},
-		{"_Leading", "_Leading"},
-		{"___Leading", "_Leading"},
-		{"trailing_", "trailing"},
-		{"trailing___", "trailing"},
-		{"a_b", "aB"},
-		{"a__b", "aB"},
-		{"a___b", "aB"},
-		{"Rpc1150", "RPC1150"},
-		{"case3_1", "case3_1"},
-		{"case3__1", "case3_1"},
-		{"IEEE802_16bit", "IEEE802_16bit"},
-		{"IEEE802_16Bit", "IEEE802_16Bit"},
-	}
-	for _, test := range tests {
-		got := lint.Name(test.name, nil, nil)
-		if got != test.want {
-			t.Errorf("lintName(%q) = %q, want %q", test.name, got, test.want)
-		}
-	}
-}
-
 // exportedType reports whether typ is an exported type.
 // It is imprecise, and will err on the side of returning true,
 // such as for composite types.
