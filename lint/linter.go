74e2417f7d843c5581af24d353cc3e924cc6c6d6
Oleksandr Redko <oleksandr.red+github@gmail.com>
2024-11-14 15:12:18 +0200

    fix: revive hangs on Windows if go.mod is not found

--- lint/linter.go
+++ lint/linter.go
@@ -184,7 +184,9 @@ func detectGoMod(dir string) (rootDir string, ver *goversion.Version, err error)
 func retrieveModFile(dir string) (string, error) {
 	const lookingForFile = "go.mod"
 	for {
-		if dir == "." || dir == "/" {
+		// filepath.Dir returns 'C:\' on Windows, and '/' on Unix
+		isRootDir := (dir == filepath.VolumeName(dir)+string(filepath.Separator))
+		if dir == "." || isRootDir {
 			return "", fmt.Errorf("did not found %q file", lookingForFile)
 		}
 
