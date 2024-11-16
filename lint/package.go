c68d9087e3435140784ba5131ab8c5c00fc8b0c7
Marcin Federowicz <marcin.federowicz@gmail.com>
2024-11-16 18:28:03 +0100

    package.go: revert to master version

--- lint/package.go
+++ lint/package.go
@@ -1,7 +1,6 @@
 package lint
 
 import (
-	"context"
 	"go/ast"
 	"go/importer"
 	"go/token"
@@ -186,37 +185,14 @@ func (p *Package) scanSortable() {
 func (p *Package) lint(rules []Rule, config Config, failures chan Failure) error {
 	p.scanSortable()
 	var wg sync.WaitGroup
-	errCh := make(chan error, 1) // Buffered channel to signal the first error
-    _, cancel := context.WithCancel(context.Background())
-
-    defer cancel() // Ensure the context is canceled at the end
-
 	for _, file := range p.files {
 		wg.Add(1)
 		go (func(file *File) {
-			defer wg.Done()
-			if err := file.lint(rules, config, failures); err != nil {
-                select {
-                case errCh <- err: // Send the error to the channel if it's empty
-                    cancel() // Signal other goroutines to stop
-                default:
-                    // If an error is already sent, do nothing
-                }
-            }	
+			file.lint(rules, config, failures)
+			wg.Done()
 		})(file)
 	}
-
-	// Wait for all goroutines to complete
-    go func() {
-        wg.Wait()
-        close(errCh)
-    }()
-
-	// Return the first error if any
-    if err, ok := <-errCh; ok {
-        return err
-    }
-
+	wg.Wait()
 	return nil
 }
 
