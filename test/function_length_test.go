0c08023e6184ecf3076c38492c9db26a31f35a2f
Oleksandr Redko <oleksandr.red+github@gmail.com>
2024-11-20 21:19:17 +0200

    test: add default case when no arguments provided to a rule (#1140)

--- test/function_length_test.go
+++ test/function_length_test.go
@@ -7,6 +7,10 @@ import (
 	"github.com/mgechev/revive/rule"
 )
 
+func TestFuncLengthDefault(t *testing.T) {
+	testRule(t, "function_length_default", &rule.FunctionLength{}, &lint.RuleConfig{})
+}
+
 func TestFuncLengthLimitsStatements(t *testing.T) {
 	testRule(t, "function_length1", &rule.FunctionLength{}, &lint.RuleConfig{
 		Arguments: []any{int64(2), int64(100)},
--- test/line_length_limit_test.go
+++ test/line_length_limit_test.go
@@ -7,6 +7,10 @@ import (
 	"github.com/mgechev/revive/rule"
 )
 
+func TestLineLengthLimitDefault(t *testing.T) {
+	testRule(t, "line_length_limit_default", &rule.LineLengthLimitRule{}, &lint.RuleConfig{})
+}
+
 func TestLineLengthLimit(t *testing.T) {
 	testRule(t, "line_length_limit", &rule.LineLengthLimitRule{}, &lint.RuleConfig{
 		Arguments: []any{int64(100)},
