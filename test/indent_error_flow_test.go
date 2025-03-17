f1a91a6bfdb1f56a794706f1812d9856dd0fe1e1
Oleksandr Redko <oleksandr.red+github@gmail.com>
2025-03-17 12:15:54 +0200

    rule: refactor and add tests for ifelse rules

---
+++ test/indent_error_flow_test.go
@@ -0,0 +1,13 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/mgechev/revive/lint"
+	"github.com/mgechev/revive/rule"
+)
+
+func TestIndentErrorFlow(t *testing.T) {
+	testRule(t, "indent_error_flow", &rule.IndentErrorFlowRule{})
+	testRule(t, "indent_error_flow_scope", &rule.IndentErrorFlowRule{}, &lint.RuleConfig{Arguments: []any{"preserveScope"}})
+}
