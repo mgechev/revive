111c586778a9a53dd94aa913dd523ff47be283b6
chavacava <salvadorcavadini+github@gmail.com>
2025-04-03 22:23:35 +0200

    fix review comments

--- testdata/golint/error_strings.go
+++ testdata/golint/error_strings.go
@@ -18,6 +18,11 @@ func g(x int) error {
 	err = errors.New("This %d is too low", x)     // MATCH /error strings should not be capitalized or end with punctuation or a newline/
 	err = errors.New("GitHub should be ok", x)
 	err = errors.New("OTP should be ok", x)
+	err = errors.New("A JSON should be not ok", x) // MATCH /error strings should not be capitalized or end with punctuation or a newline/
+	err = errors.New("H264 should be ok", x)
+	err = errors.New("I/O should be ok", x)
+	err = errors.New("U.S. should be ok", x)
+	err = errors.New("Wi-Fi should be ok", x)
 
 	// Non-regression test for issue #610
 	d.stack.Push(from)
