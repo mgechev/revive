da34efbfe2bd69189d7e7f2c4893ab24d698327b
Oleksandr Redko <oleksandr.red+github@gmail.com>
2025-04-09 14:11:21 +0300

    refactor: fix typos in comments and struct names

--- testdata/go1.24/struct_tag.go
+++ testdata/go1.24/struct_tag.go
@@ -3,7 +3,7 @@ package fixtures
 import "time"
 
 type decodeAndValidateRequest struct {
-	// BEAWRE : the flag of URLParam should match the const string URLParam
+	// BEWARE : the flag of URLParam should match the const string URLParam
 	URLParam    string `json:"-" path:"url_param" validate:"numeric"`
 	Text        string `json:"text" validate:"max=10"`
 	DefaultInt  int    `json:"defaultInt" default:"10.0"` // MATCH /field's type and default value's type mismatch/
