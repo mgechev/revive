c1cc385c3eb059549f79b89d900204aa98487d24
Oleksandr Redko <oleksandr.red+github@gmail.com>
2025-04-30 13:28:45 +0300

    chore: remove redundant typeparams for Go 1.17

--- test/receiver_naming_test.go
+++ test/receiver_naming_test.go
@@ -3,15 +3,11 @@ package test
 import (
 	"testing"
 
-	"github.com/mgechev/revive/internal/typeparams"
 	"github.com/mgechev/revive/lint"
 	"github.com/mgechev/revive/rule"
 )
 
 func TestReceiverNamingTypeParams(t *testing.T) {
-	if !typeparams.Enabled() {
-		t.Skip("type parameters are not enabled in the current build environment")
-	}
 	testRule(t, "receiver_naming_issue_669", &rule.ReceiverNamingRule{})
 }
 
