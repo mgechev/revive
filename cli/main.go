68561c50d1849a307f1165f1a2a6861536374be1
Oleksandr Redko <oleksandr.red+github@gmail.com>
2025-05-01 13:05:29 +0300

    fail

--- cli/main.go
+++ cli/main.go
@@ -33,7 +33,7 @@ var (
 
 func fail(err string) {
 	fmt.Fprintln(os.Stderr, err)
-	os.Exit(1) //revive:disable-line:deep-exit
+	os.Exit(1)
 }
 
 // RunRevive runs the CLI for revive.
@@ -87,7 +87,7 @@ func RunRevive(extraRules ...revivelib.ExtraRule) {
 		fmt.Println(output)
 	}
 
-	os.Exit(exitCode) //revive:disable-line:deep-exit
+	os.Exit(exitCode)
 }
 
 var (
