bc5ede3be808761387bcbec7c2da37e2f439771d
Oleksandr Redko <oleksandr.red+github@gmail.com>
2025-05-19 19:22:06 +0300

    revive:disable-next-line

--- rule/error_strings_test.go
+++ rule/error_strings_test.go
@@ -34,7 +34,8 @@ func TestErrorStringsRule_Configure(t *testing.T) {
 		{
 			name:      "Invalid function",
 			arguments: lint.Arguments{"errors."},
-			wantErr:   errors.New("found invalid custom function: errors."), //nolint:revive // error-strings: it's ok for tests
+			//revive:disable-next-line // error-strings
+			wantErr: errors.New("found invalid custom function: errors."), //nolint:revive // error-strings: it's ok for tests
 		},
 		{
 			name:      "Invalid custom function",
