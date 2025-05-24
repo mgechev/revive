0e68a4a59f477b3fbb3a6c82c2c84d0c84f41989
chavacava <salvadorcavadini+github@gmail.com>
2025-05-24 21:10:33 +0200

    explains when a false positive may occur

--- rule/unnecessary_format.go
+++ rule/unnecessary_format.go
@@ -51,21 +51,34 @@ var formattingFuncs = map[string]formattingSpec{
 	"fmt.Sprintf": {0, "fmt.Sprint or just the string itself"},
 	"fmt.Sscanf":  {1, "fmt.Sscan"},
 	// standard logging functions
-	"log.Fatalf":    {0, "log.Fatal"},
-	"log.Panicf":    {0, "log.Panic"},
-	"log.Printf":    {0, "log.Print"},
+	"log.Fatalf": {0, "log.Fatal"},
+	"log.Panicf": {0, "log.Panic"},
+	"log.Printf": {0, "log.Print"},
+	//// Variable logger possibly being the std logger
+	//// Will trigger a false positive if all the following holds:
+	////   1. the variable is not the std logger
+	////   2. the Fatalf/Panicf/Printf method expects a string as first argument
+	////      but the string is not expected to be a format string
+	////   3. the actual first argument is a string literal
+	////   4. the actual first argument does not contain a %
 	"logger.Fatalf": {0, "logger.Fatal"},
 	"logger.Panicf": {0, "logger.Panic"},
 	"logger.Printf": {0, "logger.Print"},
 	// standard testing functions
+	//// Variable t possibly being a testing.T
+	//// False positive risk: see comment on logger
 	"t.Errorf": {0, "t.Error"},
 	"t.Fatalf": {0, "t.Fatal"},
 	"t.Logf":   {0, "t.Log"},
 	"t.Skipf":  {0, "t.Skip"},
+	//// Variable b possibly being a testing.B
+	//// False positive risk: see comment on logger
 	"b.Errorf": {0, "b.Error"},
 	"b.Fatalf": {0, "b.Fatal"},
 	"b.Logf":   {0, "b.Log"},
 	"b.Skipf":  {0, "b.Skip"},
+	//// Variable f possibly being a testing.F
+	//// False positive risk: see comment on logger
 	"f.Errorf": {0, "f.Error"},
 	"f.Fatalf": {0, "f.Fatal"},
 	"f.Logf":   {0, "f.Log"},
