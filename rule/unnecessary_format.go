15901f7b055df619fd35ba97eed25a11e3b14747
chavacava <salvadorcavadini+github@gmail.com>
2025-05-24 07:43:17 +0200

    adds testing related functions

--- rule/unnecessary_format.go
+++ rule/unnecessary_format.go
@@ -42,20 +42,34 @@ type formattingSpec struct {
 }
 
 var formattingFuncs = map[string]formattingSpec{
-	"fmt.Appendf":   {1, "fmt.Append"},
-	"fmt.Errorf":    {0, "errors.New"},
-	"fmt.Fprintf":   {1, "fmt.Fprint"},
-	"fmt.Fscanf":    {1, "fmt.Fscan or fmt.Fscanln"},
-	"fmt.Printf":    {0, "fmt.Print or fmt.Println"},
-	"fmt.Scanf":     {0, "fmt.Scan"},
-	"fmt.Sprintf":   {0, "fmt.Sprint or just the string itself"},
-	"fmt.Sscanf":    {1, "fmt.Sscan"},
+	"fmt.Appendf": {1, "fmt.Append"},
+	"fmt.Errorf":  {0, "errors.New"},
+	"fmt.Fprintf": {1, "fmt.Fprint"},
+	"fmt.Fscanf":  {1, "fmt.Fscan or fmt.Fscanln"},
+	"fmt.Printf":  {0, "fmt.Print or fmt.Println"},
+	"fmt.Scanf":   {0, "fmt.Scan"},
+	"fmt.Sprintf": {0, "fmt.Sprint or just the string itself"},
+	"fmt.Sscanf":  {1, "fmt.Sscan"},
+	// standard logging functions
 	"log.Fatalf":    {0, "log.Fatal"},
 	"log.Panicf":    {0, "log.Panic"},
 	"log.Printf":    {0, "log.Print"},
 	"logger.Fatalf": {0, "logger.Fatal"},
 	"logger.Panicf": {0, "logger.Panic"},
 	"logger.Printf": {0, "logger.Print"},
+	// standard testing functions
+	"t.Errorf": {0, "t.Error"},
+	"t.Fatalf": {0, "t.Fatal"},
+	"t.Logf":   {0, "t.Log"},
+	"t.Skipf":  {0, "t.Skip"},
+	"b.Errorf": {0, "b.Error"},
+	"b.Fatalf": {0, "b.Fatal"},
+	"b.Logf":   {0, "b.Log"},
+	"b.Skipf":  {0, "b.Skip"},
+	"f.Errorf": {0, "f.Error"},
+	"f.Fatalf": {0, "f.Fatal"},
+	"f.Logf":   {0, "f.Log"},
+	"f.Skipf":  {0, "f.Skip"},
 }
 
 func (w lintUnnecessaryFormat) Visit(n ast.Node) ast.Visitor {
