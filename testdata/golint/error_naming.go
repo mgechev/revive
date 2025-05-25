a4737e935a451bcdefbe832970a737a732e911ab
ccoVeille <3875889+ccoVeille@users.noreply.github.com>
2025-05-25 16:36:22 +0200

    fix: avoid false positive for blank identifier (#1376)
    The fact that the error variable is not used is out of the scope of the rule
    
    This pattern that can be found in benchmarks and examples.
    It should be allowed.

--- testdata/golint/error_naming.go
+++ testdata/golint/error_naming.go
@@ -19,9 +19,19 @@ var (
 	e1 = fmt.Errorf("blah %d", 4) // MATCH /error var e1 should have name of the form errFoo/
 	// E2 ...
 	E2 = fmt.Errorf("blah %d", 5) // MATCH /error var E2 should have name of the form ErrFoo/
+
+	// check there is no false positive for blank identifier.
+	// This pattern that can be found in benchmarks and examples should be allowed.
+	// The fact that the error variable is not used is out of the scope of this rule.
+	_ = errors.New("ok")
 )
 
 func f() {
-	var whatever = errors.New("ok") // ok
+
+	// the linter does not check local variables, this one is valid
+	var whatever = errors.New("ok")
 	_ = whatever
+
+	// same as above with a blank identifier
+	_ = errors.New("ok")
 }
