19be1cd7156266ce222429c698410cf93aa0c373
chavacava <salvadorcavadini+github@gmail.com>
2025-05-26 09:25:44 +0200

    adds support for multiline strings modify format of failure message

--- testdata/unnecessary_format.go
+++ testdata/unnecessary_format.go
@@ -10,36 +10,36 @@ import (
 func unnecessaryFormat(t *testing.T, b *testing.B, f *testing.F) {
 	logger := log.New(nil, "", 0)
 
-	fmt.Appendf(nil, "no format") // MATCH /unnecessary use of formatting function fmt.Appendf, you can replace it with fmt.Append/
-	fmt.Errorf("no format")       // MATCH /unnecessary use of formatting function fmt.Errorf, you can replace it with errors.New/
-	fmt.Fprintf(nil, "no format") // MATCH /unnecessary use of formatting function fmt.Fprintf, you can replace it with fmt.Fprint/
-	fmt.Fscanf(nil, "no format")  // MATCH /unnecessary use of formatting function fmt.Fscanf, you can replace it with fmt.Fscan or fmt.Fscanln/
-	fmt.Printf("no format")       // MATCH /unnecessary use of formatting function fmt.Printf, you can replace it with fmt.Print or fmt.Println/
-	fmt.Scanf("no format")        // MATCH /unnecessary use of formatting function fmt.Scanf, you can replace it with fmt.Scan/
-	fmt.Sprintf("no format")      // MATCH /unnecessary use of formatting function fmt.Sprintf, you can replace it with fmt.Sprint or just the string itself/
-	fmt.Sscanf("", "no format")   // MATCH /unnecessary use of formatting function fmt.Sscanf, you can replace it with fmt.Sscan/
+	fmt.Appendf(nil, "no format") // MATCH /unnecessary use of formatting function "fmt.Appendf", you can replace it with "fmt.Append"/
+	fmt.Errorf("no format")       // MATCH /unnecessary use of formatting function "fmt.Errorf", you can replace it with "errors.New"/
+	fmt.Fprintf(nil, "no format") // MATCH /unnecessary use of formatting function "fmt.Fprintf", you can replace it with "fmt.Fprint"/
+	fmt.Fscanf(nil, "no format")  // MATCH /unnecessary use of formatting function "fmt.Fscanf", you can replace it with "fmt.Fscan" or "fmt.Fscanln"/
+	fmt.Printf("no format")       // MATCH /unnecessary use of formatting function "fmt.Printf", you can replace it with "fmt.Print" or "fmt.Println"/
+	fmt.Scanf("no format")        // MATCH /unnecessary use of formatting function "fmt.Scanf", you can replace it with "fmt.Scan"/
+	fmt.Sprintf("no format")      // MATCH /unnecessary use of formatting function "fmt.Sprintf", you can replace it with "fmt.Sprint" or just the string itself"/
+	fmt.Sscanf("", "no format")   // MATCH /unnecessary use of formatting function "fmt.Sscanf", you can replace it with "fmt.Sscan"/
 	// standard logging functions
-	log.Fatalf("no format")    // MATCH /unnecessary use of formatting function log.Fatalf, you can replace it with log.Fatal/
-	log.Panicf("no format")    // MATCH /unnecessary use of formatting function log.Panicf, you can replace it with log.Panic/
-	log.Printf("no format")    // MATCH /unnecessary use of formatting function log.Printf, you can replace it with log.Print/
-	logger.Fatalf("no format") // MATCH /unnecessary use of formatting function logger.Fatalf, you can replace it with logger.Fatal/
-	logger.Panicf("no format") // MATCH /unnecessary use of formatting function logger.Panicf, you can replace it with logger.Panic/
-	logger.Printf("no format") // MATCH /unnecessary use of formatting function logger.Printf, you can replace it with logger.Print/
+	log.Fatalf("no format")    // MATCH /unnecessary use of formatting function "log.Fatalf", you can replace it with "log.Fatal"/
+	log.Panicf("no format")    // MATCH /unnecessary use of formatting function "log.Panicf", you can replace it with "log.Panic"/
+	log.Printf("no format")    // MATCH /unnecessary use of formatting function "log.Printf", you can replace it with "log.Print"/
+	logger.Fatalf("no format") // MATCH /unnecessary use of formatting function "logger.Fatalf", you can replace it with "logger.Fatal"/
+	logger.Panicf("no format") // MATCH /unnecessary use of formatting function "logger.Panicf", you can replace it with "logger.Panic"/
+	logger.Printf("no format") // MATCH /unnecessary use of formatting function "logger.Printf", you can replace it with "logger.Print"/
 	// standard testing functions
-	t.Errorf("no format") // MATCH /unnecessary use of formatting function t.Errorf, you can replace it with t.Error/
-	t.Fatalf("no format") // MATCH /unnecessary use of formatting function t.Fatalf, you can replace it with t.Fatal/
-	t.Logf("no format")   // MATCH /unnecessary use of formatting function t.Logf, you can replace it with t.Log/
-	t.Skipf("no format")  // MATCH /unnecessary use of formatting function t.Skipf, you can replace it with t.Skip/
-	b.Errorf("no format") // MATCH /unnecessary use of formatting function b.Errorf, you can replace it with b.Error/
-	b.Fatalf("no format") // MATCH /unnecessary use of formatting function b.Fatalf, you can replace it with b.Fatal/
-	b.Logf("no format")   // MATCH /unnecessary use of formatting function b.Logf, you can replace it with b.Log/
-	b.Skipf("no format")  // MATCH /unnecessary use of formatting function b.Skipf, you can replace it with b.Skip/
-	f.Errorf("no format") // MATCH /unnecessary use of formatting function f.Errorf, you can replace it with f.Error/
-	f.Fatalf("no format") // MATCH /unnecessary use of formatting function f.Fatalf, you can replace it with f.Fatal/
-	f.Logf("no format")   // MATCH /unnecessary use of formatting function f.Logf, you can replace it with f.Log/
-	f.Skipf("no format")  // MATCH /unnecessary use of formatting function f.Skipf, you can replace it with f.Skip/
+	t.Errorf("no format") // MATCH /unnecessary use of formatting function "t.Errorf", you can replace it with "t.Error"/
+	t.Fatalf("no format") // MATCH /unnecessary use of formatting function "t.Fatalf", you can replace it with "t.Fatal"/
+	t.Logf("no format")   // MATCH /unnecessary use of formatting function "t.Logf", you can replace it with "t.Log"/
+	t.Skipf("no format")  // MATCH /unnecessary use of formatting function "t.Skipf", you can replace it with "t.Skip"/
+	b.Errorf("no format") // MATCH /unnecessary use of formatting function "b.Errorf", you can replace it with "b.Error"/
+	b.Fatalf("no format") // MATCH /unnecessary use of formatting function "b.Fatalf", you can replace it with "b.Fatal"/
+	b.Logf("no format")   // MATCH /unnecessary use of formatting function "b.Logf", you can replace it with "b.Log"/
+	b.Skipf("no format")  // MATCH /unnecessary use of formatting function "b.Skipf", you can replace it with "b.Skip"/
+	f.Errorf("no format") // MATCH /unnecessary use of formatting function "f.Errorf", you can replace it with "f.Error"/
+	f.Fatalf("no format") // MATCH /unnecessary use of formatting function "f.Fatalf", you can replace it with "f.Fatal"/
+	f.Logf("no format")   // MATCH /unnecessary use of formatting function "f.Logf", you can replace it with "f.Log"/
+	f.Skipf("no format")  // MATCH /unnecessary use of formatting function "f.Skipf", you can replace it with "f.Skip"/
 	// standard trace functions
-	trace.Logf(nil, "http", "no format", nil) // MATCH /unnecessary use of formatting function trace.Logf, you can replace it with trace.Log/
+	trace.Logf(nil, "http", "no format", nil) // MATCH /unnecessary use of formatting function "trace.Logf", you can replace it with "trace.Log"/
 
 	fmt.Appendf(nil, "format %d", 0)
 	fmt.Errorf("format %d", 0)
@@ -71,4 +71,11 @@ func unnecessaryFormat(t *testing.T, b *testing.B, f *testing.F) {
 	f.Skipf("format %d", 0)
 	// standard trace functions
 	trace.Logf(nil, "http", "format %d", nil)
+
+	// test with multiline string argument
+	// MATCH:77 /unnecessary use of formatting function "fmt.Appendf", you can replace it with "fmt.Append"/
+	fmt.Appendf(nil, `no 
+		format`)
+	fmt.Appendf(nil, `format 
+	%d`, 0)
 }
