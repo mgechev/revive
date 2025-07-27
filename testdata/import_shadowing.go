32b4f4f0ee2abf60484b563f90b0d38cd5fe9f4a
Baranov Victor <bar.victor.2002@gmail.com>
2025-07-27 22:47:28 +0300

    add 'v12345' case

--- testdata/import_shadowing.go
+++ testdata/import_shadowing.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	str "strings"
 	"v1"
+	"v12345"
 	"math/rand/v2"
 	randv2 "math/rand/v2"
 )
@@ -49,6 +50,7 @@ func titi() {
 
 func testVer() {
 	v1 := "" // MATCH /The name 'v1' shadows an import name/
+	v12345 := "" // MATCH /The name 'v12345' shadows an import name/
 	v2 := ""
 	rand := "" // MATCH /The name 'rand' shadows an import name/
 	randv2 := "" // MATCH /The name 'randv2' shadows an import name/
