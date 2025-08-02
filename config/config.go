3f4591cd9aca5716a1a9d669b268a33ea266855b
chavacava <salvadorcavadini+github@gmail.com>
2025-08-02 08:04:18 +0200

    feature: new rule identical-switch-conditions (#1451)

--- config/config.go
+++ config/config.go
@@ -105,6 +105,7 @@ var allRules = append([]lint.Rule{
 	&rule.UnnecessaryFormatRule{},
 	&rule.UseFmtPrintRule{},
 	&rule.EnforceSwitchStyleRule{},
+	&rule.IdenticalSwitchConditionsRule{},
 }, defaultRules...)
 
 // allFormatters is a list of all available formatters to output the linting results.
