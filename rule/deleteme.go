ce4d16d9d7cde6d9f8135405ad64f3ab4100a6de
chavacava <salvadorcavadini+github@gmail.com>
2025-08-16 07:13:29 +0200

    add explanation of the rule "philosophy"

--- rule/deleteme.go
+++ rule/deleteme.go
@@ -36,46 +36,69 @@ type lintDeletemeRule struct {
 	onFailure func(lint.Failure)
 }
 
+var wgDonePicker = func(n ast.Node) bool {
+	call, ok := n.(*ast.CallExpr)
+	result := ok && astutils.IsPkgDotName(call.Fun, "wg", "Done")
+	return result
+}
+
+// This visitor searches AST subtrees with the following form
+// wg.Add(...)
+// ...
+//
+//	go func (...) {
+//	   ...
+//	   wg.Done // or defer wg.Done
+//	   ...
+//	}
+//
+// Warning: the analisys only looks for exactly wg.Add and wg.Done, that means
+// calls to Add and Done on a WaitGroup struct withing a variable named differently than wg will be ignored
+// This simplification avoids requiring type information while still makes the rule work in most of the cases
+// (Who names a wait group differently than wg ?!)
 func (w *lintDeletemeRule) Visit(node ast.Node) ast.Visitor {
+	// Only interested in blocks of statements
 	block, ok := node.(*ast.BlockStmt)
 	if !ok {
-		return w
+		return w // not a block of statements
 	}
 
+	// Once in a block of statements
+	// we will iterate over all statemets in search for wg.Add()
 	for i, stmt := range block.List {
-		//println(">>>> block stmt:", astutils.GoFmt(stmt))
 		expr, ok := stmt.(*ast.ExprStmt)
 		if !ok {
-			continue
+			continue // not an expression statements thus not a function call
 		}
+
+		// Lets check if the expression statement is a call to wg.Add
 		call, ok := expr.X.(*ast.CallExpr)
 		notACallToWgAdd := !ok || !astutils.IsPkgDotName(call.Fun, "wg", "Add")
 		if notACallToWgAdd {
-			//println(">>>> CONTINUE main")
 			continue
 		}
-		//println(">>>> FOUND wg.Add lets check next statements")
+
+		// Here we have identified a call to wg.Add
+		// Let's iterate over the statements that follow the wg.Add
+		// to see if there is a go statement that runs a goroutine with a wg.Done
 		for i++; i < len(block.List); i++ {
 			stmt := block.List[i]
-			//println(">>>> next stmt:", astutils.GoFmt(stmt))
+			// looking for a go statement
 			goStmt, ok := stmt.(*ast.GoStmt)
 			if !ok {
-				//println(">>>> CONTINUE not a go stmt")
-				continue
-			}
-			funcLit, ok := goStmt.Call.Fun.(*ast.FuncLit)
-			//println(">>>> funclit:", astutils.GoFmt(funcLit))
-			if !ok {
-				continue
-			}
-			picker := func(n ast.Node) bool {
-				call, ok := n.(*ast.CallExpr)
-				result := ok && astutils.IsPkgDotName(call.Fun, "wg", "Done")
-				//println(">>>> picking on:", astutils.GoFmt(n), result)
-				return result
+				continue // not a go statement
 			}
 
-			found := astutils.PickNodes(funcLit.Body, picker)
+			// here we found a the go statement
+			// now let's check is the go statement is applied to a function literal that contains a wg.Done
+			funcLit, ok := goStmt.Call.Fun.(*ast.FuncLit)
+			if !ok {
+				continue // the go statements runs a function defined elsewhere
+			}
+
+			// here we found a go statement running a function literal
+			// now we will look for a wg.Done inside the body of the function literal
+			found := astutils.PickNodes(funcLit.Body, wgDonePicker)
 			if len(found) > 0 {
 				w.onFailure(lint.Failure{
 					Confidence: 1,
