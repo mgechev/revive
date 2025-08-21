d16156f7470293ec4365ab928ee87d451d698709
Boris Wolf <bowolf@cisco.com>
2025-08-21 10:38:11 -0700

    more test cases

--- testdata/secrets_serialization_default.go
+++ testdata/secrets_serialization_default.go
@@ -1,10 +1,12 @@
 package fixtures
 
 type SecretsSerializationDefault struct {
-	Foo         string
-	Bar         string
-	BearerToken string // MATCH /Struct field 'BearerToken' may contain secrets but is not excluded from JSON serialization (missing `json:"-"`)/
-	AuthToken   string `json:"-"`
-	Apikey      string //MATCH /Struct field 'Apikey' may contain secrets but is not excluded from JSON serialization (missing `json:"-"`)/
-	credential  string
+	Foo          string
+	Bar          string
+	BearerToken  string // MATCH /Struct field 'BearerToken' may contain secrets but is not excluded from JSON serialization (missing `json:"-"`)/
+	AuthToken    string `json:"-"`
+	Apikey       string //MATCH /Struct field 'Apikey' may contain secrets but is not excluded from JSON serialization (missing `json:"-"`)/
+	credential   string
+	ClientSecret string `json:"-" anotherTagName:"something, somethingElse"`
+	Password     string `anotherTagName:"something, somethingElse"` //MATCH /Struct field 'Password' may contain secrets but is not excluded from JSON serialization (missing `json:"-"`)/
 }
