2be99a3b700a8233c1e6b06404d9b74addd8c002
chavacava <salvadorcavadini+github@gmail.com>
2025-08-23 16:46:18 +0200

    test not passing

--- rule/use_waitgroup_go.go
+++ rule/use_waitgroup_go.go
@@ -26,7 +26,19 @@ func (*UseWaitGroupGoRule) Apply(file *lint.File, _ lint.Arguments) []lint.Failu
 		onFailure: onFailure,
 	}
 
-	ast.Walk(w, file.AST)
+	for _, decl := range file.AST.Decls {
+		fn, ok := decl.(*ast.FuncDecl)
+		if !ok {
+			continue
+		}
+
+		if fn.Body == nil {
+			continue // external (no-Go) function
+		}
+
+		ast.Walk(w, fn.Body)
+		//w.analyzeBlock(fn.Body)
+	}
 
 	return failures
 }
@@ -44,6 +56,61 @@ var wgDonePicker = func(n ast.Node) bool {
 	call, ok := n.(*ast.CallExpr)
 	result := ok && astutils.IsPkgDotName(call.Fun, "wg", "Done")
 	return result
+}
+
+func (w *lintUseWaitGroupGo) analyzeBlock(b *ast.BlockStmt) {
+	// we will iterate over all statements in search for wg.Add()
+	stmts := b.List
+	for i := 0; i < len(stmts); i++ {
+		stmt := stmts[i]
+
+		expr, ok := stmt.(*ast.ExprStmt)
+		if !ok {
+			continue // not an expression statements thus not a function call
+		}
+
+		// Lets check if the expression statement is a call to wg.Add
+		call, ok := expr.X.(*ast.CallExpr)
+		notACallToWgAdd := !ok || !astutils.IsPkgDotName(call.Fun, "wg", "Add")
+		if notACallToWgAdd {
+			continue
+		}
+
+		// Here we have identified a call to wg.Add
+		// Let's iterate over the statements that follow the wg.Add
+		// to see if there is a go statement that runs a goroutine with a wg.Done
+		//
+		// wg.Add is the i-th statement of block.List
+		// we will iterate from the (i+1)-th statement up to the last statement of block.List
+		for i++; i < len(stmts); i++ {
+			stmt := stmts[i]
+			// looking for a go statement
+			goStmt, ok := stmt.(*ast.GoStmt)
+			if !ok {
+				continue // not a go statement
+			}
+
+			// here we found a the go statement
+			// now let's check is the go statement is applied to a function literal that contains a wg.Done
+			funcLit, ok := goStmt.Call.Fun.(*ast.FuncLit)
+			if !ok {
+				continue // the go statements runs a function defined elsewhere
+			}
+
+			// here we found a go statement running a function literal
+			// now we will look for a wg.Done inside the body of the function literal
+			found := astutils.PickNodes(funcLit.Body, wgDonePicker) // TODO use SeekNode
+			if len(found) > 0 {
+				w.onFailure(lint.Failure{
+					Confidence: 1,
+					Node:       call,
+					Category:   lint.FailureCategoryCodeStyle,
+					Failure:    "replace wg.Add()...go {...wg.Done()...} with wg.Go(...)",
+				})
+				break
+			}
+		}
+	}
 }
 
 // This visitor searches AST subtrees with the following form
@@ -68,8 +135,9 @@ func (w *lintUseWaitGroupGo) Visit(node ast.Node) ast.Visitor {
 	}
 
 	// Once in a block of statements
-	// we will iterate over all statemets in search for wg.Add()
-	for i, stmt := range block.List {
+	// we will iterate over all statements in search for wg.Add()
+	for i := 0; i < len(block.List); i++ {
+		stmt := block.List[i]
 		expr, ok := stmt.(*ast.ExprStmt)
 		if !ok {
 			continue // not an expression statements thus not a function call
@@ -85,6 +153,9 @@ func (w *lintUseWaitGroupGo) Visit(node ast.Node) ast.Visitor {
 		// Here we have identified a call to wg.Add
 		// Let's iterate over the statements that follow the wg.Add
 		// to see if there is a go statement that runs a goroutine with a wg.Done
+		//
+		// wg.Add is the i-th statement of block.List
+		// we will iterate from the (i+1)-th statement up to the last statement of block.List
 		for i++; i < len(block.List); i++ {
 			stmt := block.List[i]
 			// looking for a go statement
@@ -111,8 +182,10 @@ func (w *lintUseWaitGroupGo) Visit(node ast.Node) ast.Visitor {
 					Failure:    "replace wg.Add()...go {...wg.Done()...} with wg.Go(...)",
 				})
 			}
+
+			return w
 		}
 	}
 
-	return w
+	return nil
 }
