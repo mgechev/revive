dfaddc18f3892ce3bf15ad8d246b80f9449ea4c4
chavacava <salvadorcavadini+github@gmail.com>
2025-08-26 18:51:41 +0200

    Update rule/use_waitgroup_go.go
    Co-authored-by: Oleksandr Redko <oleksandr.red+github@gmail.com>

--- rule/use_waitgroup_go.go
+++ rule/use_waitgroup_go.go
@@ -78,7 +78,7 @@ func (w *lintUseWaitGroupGo) Visit(node ast.Node) ast.Visitor {
 // Warning: the analysis only looks for exactly wg.Add and wg.Done, that means
 // calls to Add and Done on a WaitGroup struct within a variable named differently than wg will be ignored
 // This simplification avoids requiring type information while still makes the rule work in most of the cases.
-// (Who names a wait group differently than wg ?!)
+// This rule assumes the WaitGroup variable is named 'wg', which is the common convention.
 func (w *lintUseWaitGroupGo) analyzeBlock(b *ast.BlockStmt) {
 	// we will iterate over all statements in search for wg.Add()
 	stmts := b.List
