2db7034aca77795c4ef6489c370974a0ea5d42ec
chavacava <salvadorcavadini+github@gmail.com>
2025-08-26 19:04:46 +0200

    feature: new rule `use-waitgroup-go` (#1484)

---
+++ testdata/go1.25/use_waitgroup_go.go
@@ -0,0 +1,53 @@
+package fixtures
+
+import (
+	"sync"
+)
+
+func useWaitGroupGo() {
+	wg := sync.WaitGroup{}
+
+	wg.Add(1) // MATCH /replace wg.Add()...go {...wg.Done()...} with wg.Go(...)/
+	go func() {
+		defer wg.Done()
+		doSomething()
+	}()
+
+	wg.Add(1) // MATCH /replace wg.Add()...go {...wg.Done()...} with wg.Go(...)/
+	go func() {
+		doSomething()
+		wg.Done()
+	}()
+
+	// from golang.org/x/tools/go/packages/packages.go/parseFiles
+	for i, file := range filenames {
+		wg.Add(1) // MATCH /replace wg.Add()...go {...wg.Done()...} with wg.Go(...)/
+		go func(i int, filename string) {
+			parsed[i], errors[i] = ld.parseFile(filename)
+			wg.Done()
+		}(i, file)
+	}
+	wg.Wait()
+
+	// from kubernetes/pkg/kubelet/cm/devicemanager/manager_test.go/TestGetTopologyHintsWithUpdates
+	// notice the rule spots a wg.Add(2) (vs wg.Add(1)) therefore using wg.Go is possible but requires
+	// replacing the wg.Add and the next two go statements with two wg.Go
+
+	wg.Add(2) // MATCH /replace wg.Add()...go {...wg.Done()...} with wg.Go(...)/
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < test.count; i++ {
+			// simulate the device plugin to send device updates
+			mimpl.genericDeviceUpdateCallback(testResourceName, devs)
+		}
+		updated.Store(true)
+	}()
+	go func() {
+		defer wg.Done()
+		for !updated.Load() {
+			test.testfunc(mimpl)
+		}
+	}()
+	wg.Wait()
+}
