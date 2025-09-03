d6ed995e9054bb7715b08413e8df4b6656b221b7
chavacava <salvadorcavadini+github@gmail.com>
2025-09-03 22:22:09 +0200

    feature: new rule `waitgroup-done-in-waitgroup-do`

--- config/config.go
+++ config/config.go
@@ -114,6 +114,7 @@ var allRules = append([]lint.Rule{
 	&rule.UseWaitGroupGoRule{},
 	&rule.UnsecureURLSchemeRule{},
 	&rule.InefficientMapLookupRule{},
+	&rule.WaitGroupDoneInWaitGroupGoRule{},
 }, defaultRules...)
 
 // allFormatters is a list of all available formatters to output the linting results.
---
+++ rule/waitgroup_done_in_waitgroup_go.go
@@ -0,0 +1,89 @@
+package rule
+
+import (
+	"go/ast"
+
+	"github.com/mgechev/revive/internal/astutils"
+	"github.com/mgechev/revive/lint"
+)
+
+// WaitGroupDoneInWaitGroupGoRule spots Go idioms that might be rewritten using WaitGroup.Go.
+type WaitGroupDoneInWaitGroupGoRule struct{}
+
+// Apply applies the rule to given file.
+func (*WaitGroupDoneInWaitGroupGoRule) Apply(file *lint.File, _ lint.Arguments) []lint.Failure {
+	if !file.Pkg.IsAtLeastGoVersion(lint.Go125) {
+		return nil // skip analysis if Go version < 1.25
+	}
+
+	var failures []lint.Failure
+
+	onFailure := func(failure lint.Failure) {
+		failures = append(failures, failure)
+	}
+
+	w := &lintWaitGroupDoneInWaitGroupGo{
+		onFailure: onFailure,
+	}
+
+	// Iterate over declarations looking for function declarations
+	for _, decl := range file.AST.Decls {
+		fn, ok := decl.(*ast.FuncDecl)
+		if !ok {
+			continue // not a function
+		}
+
+		if fn.Body == nil {
+			continue // external (no-Go) function
+		}
+
+		// Analyze the function body
+		ast.Walk(w, fn.Body)
+	}
+
+	return failures
+}
+
+// Name returns the rule name.
+func (*WaitGroupDoneInWaitGroupGoRule) Name() string {
+	return "waitgroup-done-in-waitgroup-go"
+}
+
+type lintWaitGroupDoneInWaitGroupGo struct {
+	onFailure func(lint.Failure)
+}
+
+func (w *lintWaitGroupDoneInWaitGroupGo) Visit(node ast.Node) ast.Visitor {
+	call, ok := node.(*ast.CallExpr)
+	if !ok {
+		return w // not a call of statements
+	}
+
+	if !astutils.IsPkgDotName(call.Fun, "wg", "Go") {
+		return w // not a call to wg.Go
+	}
+
+	if len(call.Args) != 1 {
+		return nil // no argument (impossible)
+	}
+
+	funcLit, ok := call.Args[0].(*ast.FuncLit)
+	if !ok {
+		return nil // the argument is not a function literal
+	}
+
+	// search a wg.Done in the body of the function literal
+	wgDone := astutils.SeekNode[*ast.CallExpr](funcLit.Body, wgDonePicker)
+	if wgDone == nil {
+		return nil // there is no a call to wg.Done in the call to wg.Do
+	}
+
+	w.onFailure(lint.Failure{
+		Confidence: 1,
+		Node:       wgDone,
+		Category:   lint.FailureCategoryErrors,
+		Failure:    "wg.Done not necessary when using wg.Go",
+	})
+
+	return nil
+}
