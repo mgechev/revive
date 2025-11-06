a94c8d048e13b1a3c1af6ba7d9246cf6cd4a280f
Oleksandr Redko <oleksandr.red+github@gmail.com>
2025-11-06 18:14:07 +0200

    use tools

---
+++ tools/pinversion.go
@@ -0,0 +1,7 @@
+// Package tools is a placeholder for tooling imports, and should not be imported in production code.
+// Despite `go tool` this package is needed to workaround the fact that renovate can't update indirect dependencies.
+package tools
+
+import (
+	_ "github.com/golangci/golangci-lint/v2/pkg/exitcodes"
+)
