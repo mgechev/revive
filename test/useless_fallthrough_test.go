480ab4f1ae16800c615df5c09580b5b5d2e2f607
Oleksandr Redko <oleksandr.red+github@gmail.com>
2025-11-06 12:50:18 +0200

    docs: fix typos (#1553)

--- test/useless_fallthrough_test.go
+++ test/useless_fallthrough_test.go
@@ -6,6 +6,6 @@ import (
 	"github.com/mgechev/revive/rule"
 )
 
-func TestUselessFallTrhough(t *testing.T) {
+func TestUselessFallthrough(t *testing.T) {
 	testRule(t, "useless_fallthrough", &rule.UselessFallthroughRule{})
 }
