7f59acb9817526a3e2e8c1c040b82e3b69f316f1
Acollie <alex.collie@muzz.com>
2025-11-17 17:59:23 +0000

    FIX: tests

--- testdata/epoch_naming.go
+++ testdata/epoch_naming.go
@@ -3,12 +3,12 @@ package testdata
 import "time"
 
 var (
-	creation        = time.Now().Unix() // MATCH /var creation should have a suffix Sec, Second, Seconds/
+	creation        = time.Now().Unix() // MATCH /var creation should have one of these suffixes: Sec, Second, Seconds/
 	creationSeconds = time.Now().Unix()
 	createdAtSec    = time.Now().Unix()
 	loginTimeMilli  = time.Now().UnixMilli()
-	m               = time.Now().UnixMilli() // MATCH /var m should have a suffix Milli, Ms/
-	t               = time.Now().UnixNano()  // MATCH /var t should have a suffix Nano, Ns/
+	m               = time.Now().UnixMilli() // MATCH /var m should have one of these suffixes: Milli, Ms/
+	t               = time.Now().UnixNano()  // MATCH /var t should have one of these suffixes: Nano, Ns/
 	tNano           = time.Now().UnixNano()
 	epochNano       = time.Now().UnixNano()
 
@@ -26,14 +26,14 @@ var (
 	timestampUs           = time.Now().UnixMicro()
 	timestampMicrosecond  = time.Now().UnixMicro()
 	timestampMicroseconds = time.Now().UnixMicro()
-	badMicroValue         = time.Now().UnixMicro() // MATCH /var badMicroValue should have a suffix Micro, Microsecond, Microseconds, Us/
-	invalidUsValue        = time.Now().UnixMicro() // MATCH /var invalidUsValue should have a suffix Micro, Microsecond, Microseconds, Us/
+	badMicroValue         = time.Now().UnixMicro() // MATCH /var badMicroValue should have one of these suffixes: Micro, Microsecond, Microseconds, Us/
+	invalidUsValue        = time.Now().UnixMicro() // MATCH /var invalidUsValue should have one of these suffixes: Micro, Microsecond, Microseconds, Us/
 
 	// Wrong unit suffix - using suffix for different time unit
-	badMs         = time.Now().Unix()      // MATCH /var badMs should have a suffix Sec, Second, Seconds/
-	wrongSec      = time.Now().UnixMilli() // MATCH /var wrongSec should have a suffix Milli, Ms/
-	wrongNano     = time.Now().Unix()      // MATCH /var wrongNano should have a suffix Sec, Second, Seconds/
-	confusedMicro = time.Now().UnixNano()  // MATCH /var confusedMicro should have a suffix Nano, Ns/
+	badMs         = time.Now().Unix()      // MATCH /var badMs should have one of these suffixes: Sec, Second, Seconds/
+	wrongSec      = time.Now().UnixMilli() // MATCH /var wrongSec should have one of these suffixes: Milli, Ms/
+	wrongNano     = time.Now().Unix()      // MATCH /var wrongNano should have one of these suffixes: Sec, Second, Seconds/
+	confusedMicro = time.Now().UnixNano()  // MATCH /var confusedMicro should have one of these suffixes: Nano, Ns/
 
 	// Edge case 3: Case variations (mixed case)
 	timeSEC     = time.Now().Unix()      // OK - SEC is valid suffix
@@ -44,17 +44,17 @@ var (
 	timeSeconds = time.Now().Unix()      // OK - Seconds is valid suffix
 
 	// Edge case 6a: Explicit type declarations
-	badExplicit     int64 = time.Now().Unix() // MATCH /var badExplicit should have a suffix Sec, Second, Seconds/
-	goodSecExplicit int64 = time.Now().Unix() // MATCH /var goodSecExplicit should have a suffix Sec, Second, Seconds/
+	badExplicit     int64 = time.Now().Unix() // MATCH /var badExplicit should have one of these suffixes: Sec, Second, Seconds/
+	goodSecExplicit int64 = time.Now().Unix() // MATCH /var goodSecExplicit should have one of these suffixes: Sec, Second, Seconds/
 	explicitSec     int64 = time.Now().Unix()
 	explicitSeconds int64 = time.Now().Unix()
 	// Edge case 6b: Suffix in middle vs end (suffix must be at the end)
-	secInMiddle  int64 = time.Now().Unix()     // MATCH /var secInMiddle should have a suffix Sec, Second, Seconds/
-	nanoInMiddle int64 = time.Now().UnixNano() // MATCH /var nanoInMiddle should have a suffix Nano, Ns/
+	secInMiddle  int64 = time.Now().Unix()     // MATCH /var secInMiddle should have one of these suffixes: Sec, Second, Seconds/
+	nanoInMiddle int64 = time.Now().UnixNano() // MATCH /var nanoInMiddle should have one of these suffixes: Nano, Ns/
 )
 
 func foo() {
-	anotherInvalid := time.Now().Unix() // MATCH /var anotherInvalid should have a suffix Sec, Second, Seconds/
+	anotherInvalid := time.Now().Unix() // MATCH /var anotherInvalid should have one of these suffixes: Sec, Second, Seconds/
 	anotherValidSec := time.Now().Unix()
 	_ = time.Now().Unix()  // assignment to blank identifier - ignored
 	_ := time.Now().Unix() // short declaration with blank identifier - should be ignored
@@ -66,11 +66,11 @@ func foo() {
 	baz()
 
 	// Edge case 2: Multiple variables in one statement
-	invalidTime := time.Now().Unix()                                // MATCH /var invalidTime should have a suffix Sec, Second, Seconds/
-	invalidMilliTime := time.Now().UnixMilli()                      // MATCH /var invalidMilliTime should have a suffix Milli, Ms/
+	invalidTime := time.Now().Unix()                                // MATCH /var invalidTime should have one of these suffixes: Sec, Second, Seconds/
+	invalidMilliTime := time.Now().UnixMilli()                      // MATCH /var invalidMilliTime should have one of these suffixes: Milli, Ms/
 	goodSec, goodMs := time.Now().Unix(), time.Now().UnixMilli()    // Both should pass
-	badNanoValue := time.Now().UnixNano()                           // MATCH /var badNanoValue should have a suffix Nano, Ns/
-	badMicroValue2 := time.Now().UnixMicro()                        // MATCH /var badMicroValue2 should have a suffix Micro, Microsecond, Microseconds, Us/
+	badNanoValue := time.Now().UnixNano()                           // MATCH /var badNanoValue should have one of these suffixes: Nano, Ns/
+	badMicroValue2 := time.Now().UnixMicro()                        // MATCH /var badMicroValue2 should have one of these suffixes: Micro, Microsecond, Microseconds, Us/
 	goodNs, goodUs := time.Now().UnixNano(), time.Now().UnixMicro() // Both should pass
 	println(invalidTime, invalidMilliTime, goodSec, goodMs, badNanoValue, badMicroValue2, goodNs, goodUs)
 }
@@ -93,7 +93,7 @@ func structTest() {
 // Regular assignment (=) should be checked
 func regularAssignment() {
 	var x int64           // declaration without initialization - OK
-	x = time.Now().Unix() // MATCH /var x should have a suffix Sec, Second, Seconds/
+	x = time.Now().Unix() // MATCH /var x should have one of these suffixes: Sec, Second, Seconds/
 	println(x)
 }
 
