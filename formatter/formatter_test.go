5e5f08c4d75bc4ebb493582cbebb1a876f8b2402
Oleksandr Redko <oleksandr.red+github@gmail.com>
2025-11-28 16:54:49 +0200

    sort stylish slice

--- formatter/formatter_test.go
+++ formatter/formatter_test.go
@@ -23,6 +23,7 @@ func TestFormatter(t *testing.T) {
 <checkstyle version="5.0">
     <file name="err.go">
       <error line="33" column="4" message="replace fmt.Errorf by errors.New (confidence 0)" severity="error" source="revive/use-errors-new"/>
+      <error line="38" column="4" message="replace fmt.Errorf by errors.New (confidence 0)" severity="error" source="revive/use-errors-new"/>
     </file>
     <file name="file.go">
       <error line="2" column="5" message="error var Exp should have name of the form ErrFoo (confidence 0)" severity="warning" source="revive/error-naming"/>
@@ -32,7 +33,8 @@ func TestFormatter(t *testing.T) {
 		"default": {
 			formatter: &formatter.Default{},
 			want: `file.go:2:5: error var Exp should have name of the form ErrFoo
-err.go:33:4: replace fmt.Errorf by errors.New`,
+err.go:33:4: replace fmt.Errorf by errors.New
+err.go:38:4: replace fmt.Errorf by errors.New`,
 		},
 		"friendly": {
 			formatter: &formatter.Friendly{},
@@ -42,10 +44,13 @@ err.go:33:4: replace fmt.Errorf by errors.New`,
   ✘  https://revive.run/r#use-errors-new  replace fmt.Errorf by errors.New
   err.go:33:4
 
-✘ 2 problems (1 error, 1 warning)
+  ✘  https://revive.run/r#use-errors-new  replace fmt.Errorf by errors.New
+  err.go:38:4
+
+✘ 3 problems (2 errors, 1 warning)
 
 Errors:
-  1  use-errors-new
+  2  use-errors-new
 
 Warnings:
   1  error-naming
@@ -54,19 +59,31 @@ Warnings:
 		},
 		"json": {
 			formatter: &formatter.JSON{},
-			want:      `[{"Severity":"warning","Failure":"error var Exp should have name of the form ErrFoo","RuleName":"error-naming","Category":"naming","Position":{"Start":{"Filename":"file.go","Offset":0,"Line":2,"Column":5},"End":{"Filename":"file.go","Offset":0,"Line":2,"Column":10}},"Confidence":0,"ReplacementLine":""},{"Severity":"error","Failure":"replace fmt.Errorf by errors.New","RuleName":"use-errors-new","Category":"errors","Position":{"Start":{"Filename":"err.go","Offset":0,"Line":33,"Column":4},"End":{"Filename":"err.go","Offset":0,"Line":33,"Column":8}},"Confidence":0,"ReplacementLine":""}]`,
+			want: "[" +
+				`{"Severity":"warning","Failure":"error var Exp should have name of the form ErrFoo","RuleName":"error-naming","Category":"naming","Position":{"Start":{"Filename":"file.go","Offset":0,"Line":2,"Column":5},"End":{"Filename":"file.go","Offset":0,"Line":2,"Column":10}},"Confidence":0,"ReplacementLine":""}` +
+				"," +
+				`{"Severity":"error","Failure":"replace fmt.Errorf by errors.New","RuleName":"use-errors-new","Category":"errors","Position":{"Start":{"Filename":"err.go","Offset":0,"Line":33,"Column":4},"End":{"Filename":"err.go","Offset":0,"Line":33,"Column":8}},"Confidence":0,"ReplacementLine":""}` +
+				"," +
+				`{"Severity":"error","Failure":"replace fmt.Errorf by errors.New","RuleName":"use-errors-new","Category":"errors","Position":{"Start":{"Filename":"err.go","Offset":0,"Line":38,"Column":4},"End":{"Filename":"err.go","Offset":0,"Line":38,"Column":9}},"Confidence":0,"ReplacementLine":""}` +
+				"]",
 		},
 		"ndjson": {
 			formatter: &formatter.NDJSON{},
-			want: `{"Severity":"warning","Failure":"error var Exp should have name of the form ErrFoo","RuleName":"error-naming","Category":"naming","Position":{"Start":{"Filename":"file.go","Offset":0,"Line":2,"Column":5},"End":{"Filename":"file.go","Offset":0,"Line":2,"Column":10}},"Confidence":0,"ReplacementLine":""}
-{"Severity":"error","Failure":"replace fmt.Errorf by errors.New","RuleName":"use-errors-new","Category":"errors","Position":{"Start":{"Filename":"err.go","Offset":0,"Line":33,"Column":4},"End":{"Filename":"err.go","Offset":0,"Line":33,"Column":8}},"Confidence":0,"ReplacementLine":""}
-`,
+			want: `{"Severity":"warning","Failure":"error var Exp should have name of the form ErrFoo","RuleName":"error-naming","Category":"naming","Position":{"Start":{"Filename":"file.go","Offset":0,"Line":2,"Column":5},"End":{"Filename":"file.go","Offset":0,"Line":2,"Column":10}},"Confidence":0,"ReplacementLine":""}` +
+				"\n" +
+				`{"Severity":"error","Failure":"replace fmt.Errorf by errors.New","RuleName":"use-errors-new","Category":"errors","Position":{"Start":{"Filename":"err.go","Offset":0,"Line":33,"Column":4},"End":{"Filename":"err.go","Offset":0,"Line":33,"Column":8}},"Confidence":0,"ReplacementLine":""}` +
+				"\n" +
+				`{"Severity":"error","Failure":"replace fmt.Errorf by errors.New","RuleName":"use-errors-new","Category":"errors","Position":{"Start":{"Filename":"err.go","Offset":0,"Line":38,"Column":4},"End":{"Filename":"err.go","Offset":0,"Line":38,"Column":9}},"Confidence":0,"ReplacementLine":""}` +
+				"\n",
 		},
 		"plain": {
 			formatter: &formatter.Plain{},
-			want: `file.go:2:5: error var Exp should have name of the form ErrFoo https://revive.run/r#error-naming
-err.go:33:4: replace fmt.Errorf by errors.New https://revive.run/r#use-errors-new
-`,
+			want: `file.go:2:5: error var Exp should have name of the form ErrFoo https://revive.run/r#error-naming` +
+				"\n" +
+				`err.go:33:4: replace fmt.Errorf by errors.New https://revive.run/r#use-errors-new` +
+				"\n" +
+				`err.go:38:4: replace fmt.Errorf by errors.New https://revive.run/r#use-errors-new` +
+				"\n",
 		},
 		"sarif": {
 			formatter: &formatter.Sarif{},
@@ -112,6 +129,26 @@ err.go:33:4: replace fmt.Errorf by errors.New https://revive.run/r#use-errors-ne
             "text": "replace fmt.Errorf by errors.New"
           },
           "ruleId": "use-errors-new"
+        },
+        {
+          "level": "error",
+          "locations": [
+            {
+              "physicalLocation": {
+                "artifactLocation": {
+                  "uri": "err.go"
+                },
+                "region": {
+                  "startColumn": 4,
+                  "startLine": 38
+                }
+              }
+            }
+          ],
+          "message": {
+            "text": "replace fmt.Errorf by errors.New"
+          },
+          "ruleId": "use-errors-new"
         }
       ],
       "tool": {
@@ -136,20 +173,24 @@ err.go:33:4: replace fmt.Errorf by errors.New https://revive.run/r#use-errors-ne
 		},
 		"stylish": {
 			formatter: &formatter.Stylish{},
-			want: `file.go
+			want: `err.go
+  (33, 4)  https://revive.run/r#use-errors-new  replace fmt.Errorf by errors.New
+  (38, 4)  https://revive.run/r#use-errors-new  replace fmt.Errorf by errors.New
+
+file.go
   (2, 5)  https://revive.run/r#error-naming  error var Exp should have name of the form ErrFoo
 
-err.go
-  (33, 4)  https://revive.run/r#use-errors-new  replace fmt.Errorf by errors.New
 
-
- ✖ 2 problems (1 error) (1 warning)`,
+ ✖ 3 problems (2 errors) (1 warning)`,
 		},
 		"unix": {
 			formatter: &formatter.Unix{},
-			want: `file.go:2:5: [error-naming] error var Exp should have name of the form ErrFoo
-err.go:33:4: [use-errors-new] replace fmt.Errorf by errors.New
-`,
+			want: "file.go:2:5: [error-naming] error var Exp should have name of the form ErrFoo" +
+				"\n" +
+				"err.go:33:4: [use-errors-new] replace fmt.Errorf by errors.New" +
+				"\n" +
+				"err.go:38:4: [use-errors-new] replace fmt.Errorf by errors.New" +
+				"\n",
 		},
 	} {
 		t.Run(name, func(t *testing.T) {
@@ -194,6 +235,23 @@ err.go:33:4: [use-errors-new] replace fmt.Errorf by errors.New
 						Filename: "err.go",
 						Line:     33,
 						Column:   8,
+					},
+				},
+			}
+			failures <- lint.Failure{
+				Failure:  "replace fmt.Errorf by errors.New",
+				RuleName: "use-errors-new",
+				Category: lint.FailureCategoryErrors,
+				Position: lint.FailurePosition{
+					Start: token.Position{
+						Filename: "err.go",
+						Line:     38,
+						Column:   4,
+					},
+					End: token.Position{
+						Filename: "err.go",
+						Line:     38,
+						Column:   9,
 					},
 				},
 			}
