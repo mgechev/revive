da2d87f64a8eff7d4656d76f01e24d86428bac53
Oleksandr Redko <oleksandr.red+github@gmail.com>
2025-12-05 18:23:51 +0200

    move var def below

--- rule/redundant_test_main_exit.go
+++ rule/redundant_test_main_exit.go
@@ -65,13 +65,12 @@ func (w *lintRedundantTestMainExit) Visit(node ast.Node) ast.Visitor {
 	}
 
 	pkg := id.Name
-	fn := fc.Sel.Name
-
 	// skip flag calls because they are commonly used in TestMain
 	if pkg == "flag" {
 		return w
 	}
 
+	fn := fc.Sel.Name
 	if isCallToExitFunction(pkg, fn, ce.Args) {
 		w.onFailure(lint.Failure{
 			Confidence: 1,
