a4eb49c885866967a74dd20133ec88e3f28cec4c
Oleksandr Redko <oleksandr.red+github@gmail.com>
2026-03-04 12:26:12 +0200

    syncset: add tests

--- internal/syncset/syncset.go
+++ internal/syncset/syncset.go
@@ -1,7 +1,11 @@
 // Package syncset provides a simple, mutex-protected set for strings.
 package syncset
 
-import "sync"
+import (
+	"maps"
+	"slices"
+	"sync"
+)
 
 // Set is a concurrency-safe set of strings.
 type Set struct {
@@ -29,4 +33,12 @@ func (s *Set) AddIfAbsent(str string) bool {
 		s.elements[str] = struct{}{}
 	}
 	return !exists
+}
+
+// Elements returns a slice of all elements in the set.
+func (s *Set) Elements() []string {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	return slices.Collect(maps.Keys(s.elements))
 }
