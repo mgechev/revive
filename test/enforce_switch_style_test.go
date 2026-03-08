9eb44e59a590eb69ff26d10a044fe20e77bd4c1b
Oleksandr Redko <oleksandr.red+github@gmail.com>
2026-03-08 19:18:32 +0200

    docs: update switch style rule arguments to kebab-case

--- test/enforce_switch_style_test.go
+++ test/enforce_switch_style_test.go
@@ -10,12 +10,12 @@ import (
 func TestEnforceSwitchStyle(t *testing.T) {
 	testRule(t, "enforce_switch_style", &rule.EnforceSwitchStyleRule{})
 	testRule(t, "enforce_switch_style_allow_no_default", &rule.EnforceSwitchStyleRule{}, &lint.RuleConfig{
-		Arguments: lint.Arguments{"allowNoDefault"},
+		Arguments: lint.Arguments{"allow-no-default"},
 	})
 	testRule(t, "enforce_switch_style_allow_not_last", &rule.EnforceSwitchStyleRule{}, &lint.RuleConfig{
-		Arguments: lint.Arguments{"allowDefaultNotLast"},
+		Arguments: lint.Arguments{"allow-default-not-last"},
 	})
 	testRule(t, "enforce_switch_style_allow_no_default_allow_not_last", &rule.EnforceSwitchStyleRule{}, &lint.RuleConfig{
-		Arguments: lint.Arguments{"allowNoDefault", "allowDefaultNotLast"},
+		Arguments: lint.Arguments{"allow-no-default", "allow-default-not-last"},
 	})
 }
