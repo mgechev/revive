97846ec71737b09520bbb5ee50a68315dcac106a
Oleksandr Redko <oleksandr.red+github@gmail.com>
2026-03-16 20:05:31 +0200

    refactor: move isCallToExitFunction to internal/astutils

--- rule/utils.go
+++ rule/utils.go
@@ -1,42 +1,12 @@
 package rule
 
 import (
-	"go/ast"
 	"go/token"
 	"regexp"
 	"strings"
 
-	"github.com/mgechev/revive/internal/astutils"
 	"github.com/mgechev/revive/lint"
 )
-
-// exitFuncChecker is a function type that checks whether a function call is an exit function.
-type exitFuncChecker func(args []ast.Expr) bool
-
-var alwaysTrue exitFuncChecker = func([]ast.Expr) bool { return true }
-
-// exitFunctions is a map of std packages and functions that are considered as exit functions.
-var exitFunctions = map[string]map[string]exitFuncChecker{
-	"os":      {"Exit": alwaysTrue},
-	"syscall": {"Exit": alwaysTrue},
-	"log": {
-		"Fatal":   alwaysTrue,
-		"Fatalf":  alwaysTrue,
-		"Fatalln": alwaysTrue,
-		"Panic":   alwaysTrue,
-		"Panicf":  alwaysTrue,
-		"Panicln": alwaysTrue,
-	},
-	"flag": {
-		"Parse": func([]ast.Expr) bool { return true },
-		"NewFlagSet": func(args []ast.Expr) bool {
-			if len(args) != 2 {
-				return false
-			}
-			return astutils.IsPkgDotName(args[1], "flag", "ExitOnError")
-		},
-	},
-}
 
 func srcLine(src []byte, p token.Position) string {
 	// Run to end of line in both directions if not at line start/end.
@@ -90,21 +60,6 @@ var directiveCommentRE = regexp.MustCompile("^//(line |extern |export |[a-z0-9]+
 
 func isDirectiveComment(line string) bool {
 	return directiveCommentRE.MatchString(line)
-}
-
-// isCallToExitFunction checks if the function call is a call to an exit function.
-func isCallToExitFunction(pkgName, functionName string, callArgs []ast.Expr) bool {
-	m, ok := exitFunctions[pkgName]
-	if !ok {
-		return false
-	}
-
-	check, ok := m[functionName]
-	if !ok {
-		return false
-	}
-
-	return check(callArgs)
 }
 
 // newInternalFailureError returns a slice of Failure with a single internal failure in it.
